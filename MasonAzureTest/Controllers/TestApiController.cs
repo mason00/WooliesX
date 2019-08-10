971493180fea2bd30c95ffe11d2c9bd029445bcb
mason00 <mason.hu@domain.com.au>
2019-08-10 10:40:53 +1000

    Add project files.

---
+++ MasonAzureTest/Controllers/TestApiController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MasonAzureTest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestApiController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult Test()
+        {
+            return Ok("OK");
+        }
+    }
+}
